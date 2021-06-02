f7fe41e2d34fafd70ece33ad686ecebc4e0c5d00
tony <azntonyzxd@gmail.com>
2021-06-02 14:08:32 -0400

    day 1

---
+++ jun_21/src/com/ss/jb/one/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ss.jb.one;
+//package is usually (.com/.gov/.org, company name, project name, part of project)
+/**
+ * @author dbaek
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ jun_21/src/exercises/Ex_2.java
@@ -0,0 +1,25 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Ex_2 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int count = 0, num, numGuess = 0;
+		num = (int) (Math.random() * 100);
+		System.out.print(num);
+		while(count < 5) {
+			System.out.println("Guess a number between 1-100: ");
+			numGuess = in.nextInt();
+			if(numGuess < (num + 10) && numGuess > (num - 10)) {
+				System.out.println("Correct answer was: " + num);
+				System.exit(0);
+			} else {
+				System.out.println("Keep guessing!");
+				count++;
+			}
+		}
+		System.out.println("Sorry you guessed too many times. The correct answer was: " + num);
+		System.exit(0);
+	}
+}
